cd13a4a8b80bc478c53b88cf6b550be017de71e2
hamidb80 <hr.bolouri@gmail.com>
2022-12-23 10:08:42 +0330

    clean data

--- src/types.nim
+++ src/types.nim
@@ -1,3 +1,5 @@
+import std/options
+
 type
   Stage* = object
     title*, img_url*, img_alt*, link*: string
@@ -10,3 +12,68 @@ type
   Site* = object
     stages*: seq[Stage]
     products*, services*: seq[Article]
+
+type
+  CV* = object
+    personal_information: PV
+    education: seq[Education]
+    professional_experiences: seq[ProfessionalXP]
+    research_interests: seq[string]
+    publications: seq[string]
+    talks: seq[Talk] 
+    posters: seq[Poster] 
+    patents: seq[Patent] 
+    teaching_experience: TeachingXP
+    skills: seq[string]
+    reference: Reference
+
+  Reference = object
+
+  Span = enum
+    start, stop
+
+  TimeSpanArray = array[Span, int]
+
+  Education = object
+    degree: string
+    `of`: string
+    span: TimeSpanArray
+    `in`: string
+    thesis: Option[string]
+
+  Name = object
+    first, last: string
+
+  FamilyStatus = enum
+    single, married
+
+  Contact = object
+    phone: int
+    email, linkedin: string
+
+  PV = object
+    name: Name
+    born: Born 
+    nationality: string
+    family_status: FamilyStatus
+    contact: Contact
+
+  Patent = object
+    center: string
+    id: int
+
+  DateUnit = enum
+    year, month, day
+
+  CivilUnit = enum
+    country, city
+
+  DateArray = array[DateUnit, int]
+  LocationArray = array[CivilUnit, string]
+
+  TeachingXP = object
+    university_courses, workshops: seq[string]
+
+  Born* = object
+    `when`: DateArray
+    where: LocationArray
